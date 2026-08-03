254f2919a451e20f2be044d95d4bf83a7bbdd9b3
mohamedsaid73888-eng <mohamedsaid73888@gmail.com>
2026-08-03 09:45:25 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .extractive import summarize, keywords, split_sentences
-from .logging_utils import RunTrace, TraceEvent
-
-__all__ = ["summarize", "keywords", "split_sentences", "RunTrace", "TraceEvent"]
